257f2d9daf51a5d29efa30ee8f867e4a0492c59a
Lê Minh Quân <marucube35@gmail.com>
2022-12-31 14:07:32 +0700

    feat: test class

--- Test.cpp
+++ Test.cpp
@@ -1,118 +1,122 @@
 #include "Test.h"
-//#include "IO.h"
-//#include "RSA.h"
-//#include "Test.h"
-//#include "Algorithm.h"
-//
-//using std::tie;
-//using std::to_string;
-//
-//void Test::startClock()
-//{
-//	start = std::chrono::steady_clock::now();
-//}
-//
-//void Test::stopClock()
-//{
-//	auto stop = std::chrono::high_resolution_clock::now();
-//	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-//	io.writeOutput("Execution time: " + std::to_string(duration.count() / 10e3) + " milliseconds\n\n");
-//}
-//
-//void Test::setBase(int base)
-//{
-//	Test::base = base;
-//}
-//
-//void Test::testOperators()
-//{
-//	BigInt a, b;
-//	for (tuple<BigInt, BigInt> test : testCases)
-//	{
-//		tie(a, b) = test;
-//#if 0
-//		io.writeOutputs(a, b, a - b, " - ", base);
-//		io.writeOutputs(a, b, a + b, " + ", base);
-//		io.writeOutputs(a, b, a == b, " == ", base);
-//		io.writeOutputs(a, b, a != b, " != ", base);
-//		io.writeOutputs(a, b, a < b, " < ", base);
-//		io.writeOutputs(a, b, a > b, " > ", base);
-//		io.writeOutputs(a, 10, a >> 10, " >> ", base);
-//		io.writeOutputs(a, 5, a << 5, " << ", base);
-//		io.writeOutputs(a, b, a / b, " / ", base);
-//		io.writeOutputs(a, b, a % b, " % ", base);
-//#endif
-//		io.writeOutputs(a, b, a * b, " * ", base);
-//	}
-//}
-//
-//void Test::testIO()
-//{
-//	BigInt a, b;
-//	for (tuple<BigInt, BigInt> test : testCases)
-//	{
-//		tie(a, b) = test;
-//
-//		io.writeOutput("Binary form of a: ", a, BigIntBase::BASE_2);
-//		//io.writeOutput("Decimal form of a: ", a, BigIntBase::BASE_10);
-//		io.writeOutput("Binary form of b: ", b, BigIntBase::BASE_2);
-//		//io.writeOutput("Decimal form of b: ", b, BigIntBase::BASE_10);
-//	}
-//}
-//
-//void Test::testRandom()
-//{
-//	for (int i = 0; i < Test::testCount; i++)
-//	{
-//		BigInt firstRandomNumber = random.next(BigInt::maxByteCount);
-//		BigInt secondRandomNumber = random.next(2, firstRandomNumber - 2);
-//
-//		io.writeOutput("First random number: ", firstRandomNumber, BigIntBase::BASE_10);
-//		io.writeOutput("Second random number: ", secondRandomNumber, BigIntBase::BASE_10);
-//	}
-//}
-//
-//void Test::testAlgorithm()
-//{
-//	BigInt a, b;
-//	for (tuple<BigInt, BigInt> test : testCases)
-//	{
-//		tie(a, b) = test;
-//
-//		BigInt m = converter.decimalStrToBigInt("93033494171281019060146861548143953189380057555617");
-//		BigInt g, x, y; tie(g, x, y) = extendedEuclidean(a, b);
-//
-//		io.writeOutputs(a, b, powMod(a, b, m), " powMod ", BigIntBase::BASE_2);
-//		io.writeOutputs(a, b, gcd(a, b), " gcd ", BigIntBase::BASE_2);
-//		io.writeOutputs(a, b, g, " extended gcd ", BigIntBase::BASE_2);
-//		io.writeOutputs(a, b, inverseMod(a, b), " invMod ", BigIntBase::BASE_2);
-//	}
-//}
-//
-//void Test::testRSA()
-//{
-//	if (BigInt::maxByteCount <= 0) return;
-//
-//	RSA rsa(BigInt::maxByteCount);
-//
-//	rsa.encryptFile("plain.txt", "cipher.txt");
-//	rsa.decryptFile("cipher.txt", "decrypted.txt");
-//}
-//
-//void Test::runTest(string type)
-//{
-//	io.clearFile("log.txt");
-//	io.clearFile("output.txt");
-//	io.readInputs(testCases, BigIntBase::BASE_2);
-//
-//	if (type == "operators")
-//		testOperators();
-//	else if (type == "random")
-//		testRandom();
-//	else if (type == "io")
-//		testIO();
-//	else if (type == "algorithm")
-//		testAlgorithm();
-//	else if (type == "rsa")
-//		testRSA();
-//}
+#include "IO.h"
+#include "RSA.h"
+#include "Converter.h"
+#include "Random.h"
+#include "Algorithm.h"
+
+using std::tie;
+using std::to_string;
+
+void Test::startClock()
+{
+	start = std::chrono::steady_clock::now();
+}
+
+void Test::stopClock()
+{
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	io.writeOutput("Execution time: " + std::to_string(duration.count() / 10e3) + " milliseconds\n\n");
+}
+
+void Test::setBase(int base)
+{
+	Test::base = base;
+}
+
+void Test::testOperators()
+{
+	BigInt a, b;
+	for (tuple<BigInt, BigInt> test : testCases)
+	{
+		tie(a, b) = test;
+#if 1
+		io.writeOutputs(a, b, a - b, " - ", base);
+		io.writeOutputs(a, b, a + b, " + ", base);
+		io.writeOutputs(a, b, a == b, " == ", base);
+		io.writeOutputs(a, b, a != b, " != ", base);
+		io.writeOutputs(a, b, a < b, " < ", base);
+		io.writeOutputs(a, b, a > b, " > ", base);
+		io.writeOutputs(a, 10, a >> 10, " >> ", base);
+		io.writeOutputs(a, 5, a << 5, " << ", base);
+		io.writeOutputs(a, b, a / b, " / ", base);
+		io.writeOutputs(a, b, a % b, " % ", base);
+		io.writeOutputs(a, b, a * b, " * ", base);
+#endif
+	}
+}
+
+void Test::testIO()
+{
+	BigInt a, b;
+	for (tuple<BigInt, BigInt> test : testCases)
+	{
+		tie(a, b) = test;
+
+		io.writeOutput("Binary form of a: ", a, BigIntBase::BASE_2);
+		io.writeOutput("Decimal form of a: ", a, BigIntBase::BASE_10);
+		io.writeOutput("Binary form of b: ", b, BigIntBase::BASE_2);
+		io.writeOutput("Decimal form of b: ", b, BigIntBase::BASE_10);
+	}
+}
+
+void Test::testRandom()
+{
+	for (int i = 0; i < Test::testCount; i++)
+	{
+		BigInt firstRandomNumber = random.next(BigInt::maxByteCount);
+		BigInt secondRandomNumber = random.next(2, firstRandomNumber - 2);
+
+		io.writeOutput("First random number: ", firstRandomNumber, base);
+		io.writeOutput("Second random number: ", secondRandomNumber, base);
+	}
+}
+
+void Test::testAlgorithm()
+{
+	BigInt a, b;
+	for (tuple<BigInt, BigInt> test : testCases)
+	{
+		tie(a, b) = test;
+
+		BigInt m = converter.decimalStrToBigInt("93033494171281019060146861548143953189380057555617");
+		BigInt g, x, y; tie(g, x, y) = extendedEuclidean(a, b);
+
+		io.writeOutputs(a, b, powMod(a, b, m), " powMod ", base);
+		io.writeOutputs(a, b, gcd(a, b), " gcd ", base);
+		io.writeOutputs(a, b, g, " extended gcd ", base);
+		io.writeOutputs(a, b, inverseMod(a, b), " invMod ", base);
+	}
+}
+
+void Test::testRSA()
+{
+	if (BigInt::maxByteCount <= 0) return;
+
+	RSA rsa(BigInt::maxByteCount);
+
+	io.clearFile("cipher.txt");
+	io.clearFile("decrypted.txt");
+
+	rsa.encryptFile("plain.txt", "cipher.txt");
+	rsa.decryptFile("cipher.txt", "decrypted.txt");
+}
+
+void Test::runTest(string type)
+{
+	io.clearFile("log.txt");
+	io.clearFile("output.txt");
+	io.readInputs(testCases, base);
+
+	if (type == "operators")
+		testOperators();
+	else if (type == "random")
+		testRandom();
+	else if (type == "io")
+		testIO();
+	else if (type == "algorithm")
+		testAlgorithm();
+	else if (type == "rsa")
+		testRSA();
+}
--- main.cpp
+++ main.cpp
@@ -4,100 +4,14 @@
 #include "Algorithm.h"
 #include "Converter.h"
 #include "RSA.h"
+#include "Test.h"
 #include "Command.h"
 #include <chrono>
 
-using std::cout;
-using std::endl;
-using std::tie;
-using namespace std::chrono;
-
-vector<tuple<BigInt, BigInt>> testCases;
-
-steady_clock::time_point start;
-
-void startClock(steady_clock::time_point& start) {
-	start = high_resolution_clock::now();
-}
-
-void stopClock(steady_clock::time_point start) {
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
-	io.writeLog("Execution time: " + std::to_string(duration.count() / 10e3) + " milliseconds\n\n");
-}
-
-void operatorTest(BigInt a, BigInt b) {
-#if 0
-	a / b;
-	a% b;
-	a* b;
-	a < b;
-	a > b;
-	a& b;
-	a | b;
-	a - b;
-	a + b;
-	a == b;
-	a != b;
-	a << 5;
-	a >> 10;
-#endif
-}
-
-void ioTest(BigInt a) {
-	io.writeOutput("Bin: " + converter.bigIntToBinaryStr(a));
-	io.writeOutput("Dec: " + converter.bigIntToDecimalStr(a));
-}
-
-void randomTest() {
-	BigInt n = 11;
-	BigInt randNum = random.next(2, n - 2);
-
-	bool isSmaller = randNum < n - 2;
-	io.writeOutput(isSmaller ? "yes" : "no");
-}
-
-void algoTest(BigInt a, BigInt b) {
-#if 0
-	BigInt m = converter.decimalStrToBigInt("3332934698137425361660362443025144786509522466962699924195150122084633952149");
-	BigInt res = powMod(a, b, m);
-	io.writeOutputs(a, b, res, " powMod ");
-
-	BigInt g = gcd(a, b);
-	io.writeOutputs(a, b, g, " gcd ");
-
-	BigInt g, x, y;
-	std::tie(g, x, y) = extendedEuclidean(a, b);
-	io.writeOutputs(a, b, g, " extended gcd ");
-
-	BigInt res = inverseMod(a, b);
-	io.writeOutputs(a, b, res, " invMod ");
-#endif
-}
-
-void test()
-{
-	io.readInputs(testCases, BigIntBase::BASE_10);
-
-	for (int i = 0; i < testCases.size(); i++) {
-		BigInt a, b;
-		tie(a, b) = testCases[i];
-
-		//operatorTest(a, b);
-		//ioTest(a);
-		//ioTest(b);
-		algoTest(a, b);
-	}
-}
-
 int main()
 {
-	io.clearFile("log.txt");
-	io.clearFile("output.txt");
-
-	Command::run();
-	//RSA::test();
-	//test();
+	Test::setBase(BigIntBase::BASE_10);
+	Test::runTest("rsa");
 
 	return 0;
 }
